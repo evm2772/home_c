025824726dba3b2fe8d339e3207c3c0455e02045
e.melkov <evm2772@gmail.com>
2024-10-25 20:11:51 +0300

    A1

--- HW_01/A1.c
+++ HW_01/A1.c
@@ -1,23 +1,11 @@
 
 #include <stdio.h>
 
-void print_spaces(int space_n)
-{   
-    int counter = 0;
-    while(counter < space_n)
-    {
-        printf(" ");
-        counter++;
-    }
-}
-
-
 int main()
 {   
-    printf("\t Let's\n");
-    printf("\t\t   go\n");
-    //printf("%20s","Hello world!");
-    //print_trailed("sssssss", 3);
+    printf("%*s\n", 9, "Let's");
+    printf("%*s\n", 12, "go");
+    printf("%*s\n", 21, "to walk");
     return 0;
 }
 
