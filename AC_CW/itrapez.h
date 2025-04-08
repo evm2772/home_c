e628a870cffd4dd4514169dc7bbc9dd05243c6e3
evm <evm2772@gmail.com>
2025-04-08 21:38:11 +0300

    Fix

--- AC_CW/itrapez.h
+++ AC_CW/itrapez.h
@@ -1,5 +1,5 @@
-#ifndef ROOTS_H
-#define ROOTS_H
+#ifndef ITRAPEZ_H
+#define ITRAPEZ_H
 double trapezoidal_rule(double (*func)(double), double a, double b, int n);
 double integral(double (*func)(double), double a, double b, double eps2);
 #endif
